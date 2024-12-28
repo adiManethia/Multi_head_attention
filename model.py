209e259fee1ff06e575636c7ed3addbaaefafb22
adi2075 <adigit2075.14111998@gmail.com>
2024-12-28 13:24:35 +0100

    model arch complete

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import math
 
 
-### Start with Input embedding
+#################_________________________Encoder_Block_________________________#################
 
 class InputEmbedding(nn.Module):
     def __init__(self, d_model: int, vocab_size: int):
@@ -172,6 +172,118 @@ class Encoder(nn.Module):
         return self.norm(x)
         
     
-    
+#################_________________________Decoder_Block_________________________#################
+
+class DecoderBlock(nn.Module):
+    def __init__(self, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_block
+        #here we have 3 residual connections
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(3)])
+        
+    def forward(self, x, encoder_output, src_mask, tgt_mask): # tgt mask is target mask, to mask the future tokens, src_mask is coming from encoder and tgt_mask is coming from decoder
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, tgt_mask))
+        x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
+        x = self.residual_connections[2](x, self.feed_forward_block)
+        return x
 
         
+class Decoder(nn.Module):
+    
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+        
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, encoder_output, src_mask, tgt_mask)
+        return self.norm(x)
+    
+#################________________________Final Linear layer_________________________#################
+# This layer will take the output of decoder and convert it into the final output and map it to the vocabulary, convert embeddings into a position in the vocabulary
+# This layer will have a linear layer and a softmax layer
+class ProjectionLayer(nn.Module):
+    
+    def __init__(self, d_model: int, vocab_size: int) -> None:
+        super().__init__()
+        self.proj = nn.Linear(d_model, vocab_size)
+        
+    def forward(self, x):
+        # (Batch, Seq_len, d_model) --> (Batch, Seq_len, vocab_size)
+        return torch.log_softmax(self.proj(x), dim=-1)  # log softmax is used to avoid underflow, it is more numerically stable
+    
+#################________________________Transformer_Model_________________________#################
+
+class Transformer(nn.Module):
+    
+    def __init__(self, encoder: Encoder, decoder: Decoder, src_embed: InputEmbedding, tgt_embed: InputEmbedding, src_pos: PositionalEncoding, tgt_pos: PositionalEncoding, projection_layer:ProjectionLayer) -> None:
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.src_pos = src_pos
+        self.tgt_pos = tgt_pos
+        self.projection_layer = projection_layer
+        
+    # define 3 methods, one to encode the source, one to decode the target and one to project
+    def encode(self, src, src_mask):
+        src = src.src_embed(src)
+        src = self.src_pos(src)
+        return self.encoder(src, src_mask)
+    
+    def decode(self, encoder_output, tgt, src_mask, tgt_mask):
+        tgt = self.tgt_embed(tgt)
+        tgt = self.tgt_pos(tgt)
+        return self.decoder(tgt, encoder_output, src_mask, tgt_mask)
+    
+    def project(self, decoder_output):
+        return self.projection_layer(decoder_output)
+    
+#################_________________________Model_Helper_Functions_________________________#################
+# final block with hyperparameters that will build transformer model
+def build_transformer(src_vocab_size: int, tgt_vocab_size: int,src_seq_len: int, tgt_seq_len:int, d_model: int= 512, N: int = 6, h: int=8,dropout: float=0.1, d_ff:int=2048) -> Transformer:   # these all are hyperparameters
+    # create the embedding layer
+    src_embed = InputEmbedding(d_model, src_vocab_size)
+    tgt_embed = InputEmbedding(d_model, tgt_vocab_size)
+    
+    # create position encoding layers
+    src_pos = PositionalEncoding(d_model, src_seq_len, dropout)
+    tgt_pos = PositionalEncoding(d_model, tgt_seq_len, dropout)
+    
+    # create the encoder blocks
+    encoder_blocks = []
+    for _ in range(N):
+        encoder_self_attention_block = MultiHeadAttentionBlock(d_model, h, dropout)
+        feed_forward_block = FeedForwardBlock(d_model, d_ff, dropout)
+        encoder_block = EncoderBlock(encoder_self_attention_block, feed_forward_block, dropout)
+        encoder_blocks.append(encoder_block)
+        
+    # create decoder blocks
+    decoder_blocks = []
+    for _ in range(N):
+        decoder_self_attention_block = MultiHeadAttentionBlock(d_model, h, dropout)
+        decoder_cross_attention_block = MultiHeadAttentionBlock(d_model, h, dropout)
+        feed_forward_block = FeedForwardBlock(d_model, d_ff, dropout)
+        decoder_block = DecoderBlock(decoder_self_attention_block, decoder_cross_attention_block, feed_forward_block, dropout)
+        decoder_blocks.append(decoder_block)
+        
+    # Creaet encoder and decoder
+    encoder = Encoder(nn.ModuleList(encoder_blocks))
+    decoder = Decoder(nn.ModuleList(decoder_blocks))
+    
+    # Create projection layer
+    projection_layer = ProjectionLayer(d_model, tgt_vocab_size)
+    
+    # Create the transformer
+    transformer = Transformer(encoder, decoder, src_embed, tgt_embed, src_pos, tgt_pos, projection_layer)
+    
+    # Initialize the parameters
+    for p in transformer.parameters():
+        if p.dim()>1:
+            nn.init.xavier_uniform_(p)
+            
+    return transformer
